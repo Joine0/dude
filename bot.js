e80a457f0cd6de78b138637312cbb0f2d66c4814
Joine0 <38402849+Joine0@users.noreply.github.com>
2018-05-28 10:51:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -210,7 +210,8 @@ if (message.content.startsWith(adminprefix + 'setgame')) {
 } else 
   if (message.content.startsWith(adminprefix + 'setname')) {
 client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`);
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
   } else
   if (message.content.startsWith(adminprefix + 'setavatar')) {
 client.user.setAvatar(argresult);
